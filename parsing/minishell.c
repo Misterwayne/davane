d84fdaede9322450691258c81db0450f7b0e6ba3
Daria <daria@MacBook-Pro-Daria.local>
2020-03-22 17:46:04 +0100

    D-parsing. Read only first line, parse only white space

---
+++ parsing/minishell.c
@@ -0,0 +1,29 @@
+#include "minishell.h"
+#include "gnl/get_next_line.h"
+
+char	**lsh_split_line(char *line)
+{
+	char **tockens;
+
+	tockens = ft_split(line, ' ');
+	return(tockens);
+}
+
+void	lsh_loop(void)
+{
+	char *line;
+	char **args;
+	int red;
+
+	write (1, ">", 1);
+	red = get_next_line(1, &line);
+	args = lsh_split_line(line);
+	free(line);
+	free(args);
+}
+
+int		main(int argc, char **argv)
+{
+	lsh_loop();
+	return (0);
+}
