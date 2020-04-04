725ec021f91c3e6d8d01e31f3855aa453468093f
Misterwayne <baidtfall319@gmail.com>
2020-04-04 19:02:11 +0200

    added 'shell' struct

--- headers/minishell.h
+++ headers/minishell.h
@@ -1,11 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: truepath <truepath@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/02 16:29:20 by truepath          #+#    #+#             */
+/*   Updated: 2020/04/04 18:16:03 by truepath         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define ADD_VAR 1
+# define CMD_FOUND 2
+# define ERROR -1
 
 #include <unistd.h>
 #include "../srcs/gnl/get_next_line.h"
-#include <string.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include "libft.h"
 #include "header.h"
 
@@ -27,14 +41,26 @@ typedef struct  s_cmd
 	char		*path;
 }				t_cmd;
 
+typedef struct	s_shell
+{
+	t_cmd		*cmd;
+	t_env		*env;
+	t_var		*var;
+	int			var_flags;
+}				t_shell;
+
 char			**ft_split(char const *s, char c);
 void			print_promt(void);
 int 			launch(char *prog, char **argv);
-void    		load_cmd(t_cmd *cmd);
+void    		load_cmd(t_shell *shell);
 char   			*pwd(void);
 void			lsh_loop(void);
 char			*get_value(t_env *env, char *line);
-int				check_var(char *line, t_var *var, t_env *env);
-int     		check_commande(t_cmd *cmd, char *line);
+int				add_var(char *line, t_shell *shell);
+int     		check_commande(t_shell *shell, char *line);
+int     		check_var(char *line);
+int				parsing_line(t_shell *shell, char **args);
+int				load_env(t_shell *shell);
+char			**lsh_split_line(char *line);
 
 #endif
