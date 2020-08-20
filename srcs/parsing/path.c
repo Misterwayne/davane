295633609f18855d9df704eaec0482871d76743e
42 <42>
2020-08-20 16:40:17 +0200

    path.c

---
+++ srcs/parsing/path.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/20 15:01:32 by mwane             #+#    #+#             */
+/*   Updated: 2020/08/20 16:38:04 by mwane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+int     launch_from_path(char **env,char **args, char *cmd)
+{
+    char **paths;
+    char *line;
+    int     i;
+
+    i = 0;
+    while (env[i])
+    {
+        if (ft_strncmp(env[i],"PATH=",5) == 0)
+            break;
+        i++;
+    }
+    paths = ft_split((env[i] + 5), ':');
+    i = 0;
+    while (paths[i])
+    {
+        line = ft_strjoin(paths[i], "/");
+        line = ft_strjoin(line, cmd);
+        if (open(line, O_RDONLY) != -1)
+            return (launch_bin(line, args));
+        i++;
+    }
+    return (0);
+}
+
+int     launch_bin(char *line, char **args)
+{
+    int     i;
+    pid_t   pid;
+
+    pid = fork();
+    if (pid < 0)
+    {
+        printf("forck error");
+        exit(1);
+    }
+    if (pid == 0)
+        i = execv(line,args);
+    else
+        waitpid(pid, NULL, 0);
+    return i;
+}
