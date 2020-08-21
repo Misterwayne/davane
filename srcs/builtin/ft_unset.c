ef32b5825d6e5c02b164cccfae803ea19cb44d2f
Daria VLASOVA <davlasov@e1r3p6.42.fr>
2020-08-21 14:08:52 +0200

    unset to delete and chain the list

--- srcs/builtin/ft_unset.c
+++ srcs/builtin/ft_unset.c
@@ -6,41 +6,43 @@
 /*   By: davlasov <davlasov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 13:28:22 by mwane             #+#    #+#             */
-/*   Updated: 2020/08/20 19:09:21 by davlasov         ###   ########.fr       */
+/*   Updated: 2020/08/21 14:07:26 by davlasov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parsing/gnl/get_next_line.h"
 #include "../../headers/minishell.h"
 
-void	search_for_var(char **argv, t_env *env)
+t_env	*search_for_var(char **argv, t_env *env)
 {
 	t_env	*tmp;
 	t_env	*next;
 	t_env	*prev;
 
+	prev = 0;
 	tmp = env;
 	while (tmp)
 	{
 		if (ft_strcmp(argv[1], tmp->key) == 0)
 		{
-			ft_printf("CHECK");
-			// prev = tmp->prev;
-			// next = tmp->next;
-			// free(tmp->key);
-			// free(tmp->next);
-			//free(tmp);
-			//prev = next;
-			// prev->next = next;
-			// next->prev = prev;
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			if (prev == 0)
+				env = next;
+			else
+				prev->next = next;
+			return (env);
 		}
+		prev = tmp;
 		tmp = tmp->next;
 	}
+	return (env);
 }
 
 int		unset(char **argv, t_shell *shell)
 {
-	//open_close(argv);
-	search_for_var(argv, shell->env);
+	shell->env = search_for_var(argv, shell->env);
 	return (0);
 }
