849a813c4c8225129549eccecf8a67a49670ac35
Daria VLASOVA <davlasov@e1r3p6.42.fr>
2020-08-21 16:02:23 +0200

    echo

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -3,37 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
+/*   By: davlasov <davlasov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/26 17:18:54 by truepath          #+#    #+#             */
-/*   Updated: 2020/08/19 18:04:01 by mwane            ###   ########.fr       */
+/*   Updated: 2020/08/21 16:01:45 by davlasov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-void	print_arguments(char **argv)
+void	print_arguments(char **argv, t_env *env)
 {
 	int i;
 
 	i = 1;
-
 	while (argv[i] != NULL)
 	{
-		ft_printf("%s", argv[i]);
+		ft_printf("%s ", argv[i]);
 		i++;
 	}
 }
 
-void open_write(char **argv)
+void open_write(char **argv, t_env *env)
 {
-	print_arguments(argv);
+	print_arguments(argv, env);
 }
 
 int		echo(char **argv, t_shell *shell)
 {
-
-	open_write(argv);
-	printf("\n");
+	open_write(argv, shell->env);
+	ft_printf("\n");
 	return (0);
 }
