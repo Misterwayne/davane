828cf2d8e47440b886d3fd1ed5c9b412a2c602ff
Daria VLASOVA <davlasov@e1r8p6.42.fr>
2020-08-25 16:48:19 +0200

    with rights

--- srcs/parsing/redirection.c
+++ srcs/parsing/redirection.c
@@ -6,51 +6,58 @@
 int					get_next_line(int fd, char **line);
 int					has_return(char *str);
 char				*join_str(const char *s1, const char *s2);
+void	print_arguments(char **argv, t_env *env);
 
-void 	ls_call(int fd)
+void 	call_binaries(int fd)
 {
 	int status;
 	char *data;
 
 	//char *args[] = {"/bin/cat", "new.c", "new.txt", 0};
-	char *args[] = {"/bin/pwd", 0};
+	char *args[] = {"/bin/ls", 0};
 	if (fork() == 0)
         {
 			dup2(fd, 1);	
 			execv(args[0], args); // child: call execv with the path and the args
+			exit(0); // not sure if it works this way
 		}
     else
         wait(&status);        // parent: wait for the child (not really necessary)
-	//write(fd, "privet", 6);
 	ft_printf("success_ls\n");
 }
 
-void	open_create_close_file(char *file)
+void	call_builtin(char **argv, int fd)
+{
+	int status;
+	char *data;
+
+	if (fork() == 0)
+    {
+		dup2(fd, 1);	
+		print_arguments(argv, 0);// child: call execv with the path and the args
+		exit(0);
+	}
+    else
+        wait(&status);        // parent: wait for the child (not really necessary)
+	printf("succ");
+}
+
+void	redirection(char **argv, char *file)
 {
 	int fd;
 
-	fd = open(file, O_TRUNC + O_RDWR | O_CREAT + O_RDWR); // begining of file
-	//fd = open(file, O_RDWR + O_APPEND | O_CREAT + O_RDWR); // enf of file
-	ls_call(fd);
+	//fd = open(file, O_TRUNC | O_CREAT | O_RDWR, S_IRUSR | S_IWUSR |  S_IRGRP | S_IROTH); // begining of file
+	fd = open(file, O_APPEND | O_CREAT | O_RDWR, S_IRUSR | S_IWUSR |  S_IRGRP | S_IROTH); // enf of file
+	call_binaries(fd); // for binaries
+	//call_builtin(argv, fd); // for builtins
 	close (fd);
 	printf("succes2\n");
-}
-
-void	redirection(char **argv)
-{
-	int fd3;
-	int fd0;
-	int reader;
-	char *data;
-
-	open_create_close_file("check.c");
-	printf("succes1\n");
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 1)
-		redirection(argv);
+		redirection(argv, "check.c");
 	else
 	{
 		printf("enter argumets");
